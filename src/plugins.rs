3cc47e0341cb63801332dc990f6040f43f490195
PROMETHIA <electriccobras@gmail.com>
2022-06-01 20:10:33 -0400

    Remove GravityScale register

--- src/plugins.rs
+++ src/plugins.rs
@@ -7,7 +7,6 @@ impl Plugin for WanderlustPlugin {
     fn build(&self, app: &mut App) {
         app.register_type::<CharacterController>()
             .register_type::<ControllerSettings>()
-            .register_type::<bevy_rapier3d::prelude::GravityScale>()
             .add_system(movement);
     }
 }
