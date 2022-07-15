5624473bbc74aa0105d69af297979479a823a899
PROMETHIA <electriccobras@gmail.com>
2022-07-15 08:23:55 -0400

    Duplicated readme to crate-level doc

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,5 @@
 #![deny(missing_docs)]
-//! # Wanderlust
-//! Wanderlust is a character controller addon. Inspired by [this excellent video](https://www.youtube.com/watch?v=qdskE8PJy6Q) and
-//! my previous attempts at creating a character controller, it is implemented on top of [Rapier physics](https://rapier.rs/)
-//! and highly customizable.
-//!
-//! ```rust,no_run
-//! # use bevy::prelude::*;
-//! use bevy_mod_wanderlust::WanderlustPlugin;
-//!
-//! App::new().add_plugins(DefaultPlugins).add_plugin(WanderlustPlugin).run()
-//! ```
-//! 
-//! Wanderlust does not handle mouselook, as it's more-or-less trivial to implement compared to movement, and would add significant complexity to build in
-//! as many projects will have vastly different requirements for mouselook. The `simple.rs` example includes an example mouselook implementation.
+#![doc = include_str!("../README.md")]
 
 mod bundles;
 mod components;
