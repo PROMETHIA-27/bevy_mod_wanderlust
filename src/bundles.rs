f98f3966a049bd50def527f9210296b9a50a1f4f
PROMETHIA <electriccobras@gmail.com>
2022-07-17 10:37:10 -0400

    Added globaltransform to bundles

--- src/bundles.rs
+++ src/bundles.rs
@@ -2,7 +2,7 @@ use super::components::*;
 use bevy::{math::*, prelude::*};
 use bevy_rapier3d::prelude::*;
 
-/// The recommended bundle for creating a character controller. Includes the necessary components for a character controller
+/// The recommended bundle for creating a first-person character controller. Includes the necessary components for a character controller
 /// as well as many physics-related components that can be used to tweak the behavior of the controller, with reasonable default
 /// values.
 #[derive(Bundle)]
@@ -19,6 +19,8 @@ pub struct FPSControllerBundle {
     pub collider: Collider,
     /// See [`Transform`].
     pub transform: Transform,
+    /// See [`GlobalTransform`].
+    pub global_transform: GlobalTransform,
     /// See [`Velocity`].
     pub velocity: Velocity,
     /// See [`GravityScale`].
@@ -75,6 +77,103 @@ impl Default for FPSControllerBundle {
             rigidbody: default(),
             collider: Collider::capsule(vec3(0.0, 0.0, 0.0), vec3(0.0, 0.5, 0.0), 0.5),
             transform: default(),
+            global_transform: default(),
+            velocity: default(),
+            gravity: GravityScale(0.0),
+            sleeping: default(),
+            ccd: default(),
+            force: default(),
+            locked_axes: default(),
+            friction: Friction {
+                coefficient: 0.0,
+                combine_rule: CoefficientCombineRule::Min,
+            },
+            damping: Damping {
+                linear_damping: 0.0,
+                angular_damping: 0.0,
+            },
+            restitution: Restitution {
+                coefficient: 0.0,
+                combine_rule: CoefficientCombineRule::Min,
+            },
+        }
+    }
+}
+
+/// A flying controller with spaceship-like controls.
+#[derive(Bundle)]
+pub struct SpaceshipControllerBundle {
+    /// See [`CharacterController`].
+    pub controller: CharacterController,
+    /// See [`ControllerSettings`].
+    pub settings: ControllerSettings,
+    /// See [`ControllerInput`].
+    pub input: ControllerInput,
+    /// See [`RigidBody`].
+    pub rigidbody: RigidBody,
+    /// See [`Collider`].
+    pub collider: Collider,
+    /// See [`Transform`].
+    pub transform: Transform,
+    /// See [`GlobalTransform`].
+    pub global_transform: GlobalTransform,
+    /// See [`Velocity`].
+    pub velocity: Velocity,
+    /// See [`GravityScale`].
+    pub gravity: GravityScale,
+    /// See [`Sleeping`].
+    pub sleeping: Sleeping,
+    /// See [`Ccd`].
+    pub ccd: Ccd,
+    /// See [`ExternalImpulse`].
+    pub force: ExternalImpulse,
+    /// See [`LockedAxes`].
+    pub locked_axes: LockedAxes,
+    /// See [`Friction`].
+    pub friction: Friction,
+    /// See [`Damping`].
+    pub damping: Damping,
+    /// See [`Restitution`].
+    pub restitution: Restitution,
+}
+
+impl Default for SpaceshipControllerBundle {
+    fn default() -> Self {
+        Self {
+            controller: default(),
+            settings: ControllerSettings {
+                acceleration: 50.0,
+                max_speed: 10.0,
+                max_acceleration_force: 10.0,
+                up_vector: Vec3::Y,
+                gravity: 25.0,
+                max_ground_angle: 45.0 * (std::f32::consts::PI / 180.0),
+                min_float_offset: -0.3,
+                max_float_offset: 0.05,
+                jump_time: 0.5,
+                jump_initial_force: 15.0,
+                jump_force: 0.0,
+                jump_stop_force: 0.3,
+                jump_decay_function: |x| (1.0 - x).sqrt(),
+                jump_skip_ground_check_duration: 0.5,
+                extra_jumps: 0,
+                coyote_time_duration: 0.16,
+                jump_buffer_duration: 0.16,
+                force_scale: vec3(1.0, 0.0, 1.0),
+                float_cast_length: 1.0,
+                float_cast_origin: vec3(0.0, 0.0, 0.0),
+                float_cast_collider: Collider::ball(0.45),
+                float_distance: 0.55,
+                float_strength: 10.0,
+                float_dampen: 0.5,
+                upright_spring_strength: 100.0,
+                upright_spring_damping: 10.0,
+            },
+            input: default(),
+            rigidbody: default(),
+            collider: Collider::capsule(vec3(0.0, 0.0, 0.0), vec3(0.0, 0.5, 0.0), 0.5),
+            transform: default(),
+            global_transform: default(),
             velocity: default(),
             gravity: GravityScale(0.0),
             sleeping: default(),
